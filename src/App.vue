4a3f1efb123b3ade290101affcb8d5b66ebbb1ee
unknown <1255617118@qq.com>
2019-07-16 00:29:28 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">123
+    <img alt="Vue logo" src="./assets/logo.png">1234
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
